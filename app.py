20390cda8203ca3421a77c677f27490e45918304
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-12 04:34:28 +0300

    k

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import requests
+from flask_cors import CORS
+
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return "Hello, world!"
+cors = CORS(app, resources={r'*': {'origins': '*'}})
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'POST':
+        id = request.json['id']
+        url = f"https://vadss.vercel.app/api/savedImages?id={id}"
+        response = requests.get(url)
+        data = response.json()        
+    # Download the image from the URL
+        imageURL =  data['url']
+        response = {'url': imageURL}
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return jsonify(response)
+    else:
+        return "YOLOv5 Model APP"
