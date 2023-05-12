ceb4bfe60cc3f46f36832e1254231f2c9124a879
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-12 06:02:31 +0300

    final

--- app.py
+++ app.py
@@ -8,13 +8,15 @@ cors = CORS(app, resources={r'*': {'origins': '*'}})
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
-        id = request.json['id']
-        url = f"https://vadss.vercel.app/api/savedImages?id={id}"
-        response = requests.get(url)
-        data = response.json()
-        url = data['url']
-        subprocess.run(['python', 'detect.py', '--source', source_url])
+       # id = request.json['id']
+        #url = f"https://vadss.vercel.app/api/savedImages?id={id}"
+        #response = requests.get(url)
+        #data = response.json()
+        #url = data['url']
     # create the response with the Access-Control-Allow-Origin header
+        url = request.json['url']
+        weights_path = 'best.pt'
+        subprocess.run(['python', 'detect.py', '--source', url, '--weights', weights_path])
         resp = jsonify({'url': url})
         resp.headers.add('Access-Control-Allow-Origin', '*')
         return resp
