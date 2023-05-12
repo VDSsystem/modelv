5ca88022603dcb23205a8fc6edc8e05a57dd70d1
AlDanah-aa1905457 <vdssystem0@gmail.com>
2023-05-12 05:16:11 +0300

    new

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import subprocess
 
 def run_detection(source_url):
-    cmd = f"python ./yolov5/detect.py --source {source_url} --weights ./best.pt"
-    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    subprocess.run(['python', 'detect.py', '--source', source_url])
     output, error = process.communicate()
     return output, error
